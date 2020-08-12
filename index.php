6a9aebfac99222035afcae9f15655c4b6f4690f0
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-12 21:03:15 +0200

    Fast blog layout

--- index.php
+++ index.php
@@ -7,9 +7,69 @@ and open the template in the editor.
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link href="index.css" rel="stylesheet" type="text/css">
+        <script src="https://kit.fontawesome.com/85b8e23c67.js" crossorigin="anonymous"></script>
         <title>PHP & MySQL Blog</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <!-- HEADER -->
+        <header>
+            <div class="top-section">
+                <a class="top-section__logo" href="">
+                    <i class="fas fa-rocket" ></i>A Random Blog
+                </a>
+                <ul class="top-section__access-buttons">
+                    <li>
+                        <a href="">
+                            <i class="fas fa-user"></i>Register
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-sign-in-alt"></i>Login
+                        </a>
+                    </li>
+                </ul> 
+            </div>
+            
+            <!-- CATEGORIES MENU -->
+            <nav class="top-navbar">
+                <a href="">Japan</a>
+                <a href="">Fitness</a>
+                <a href="">Web Dev</a>
+                <a href="">Random</a>
+            </nav>
+        </header>
+        
+        <!-- MAIN CONTAINER -->
+        <div class="main-container">
+          <main class="posts-box">
+            <article>
+                <h2>Google Chrome</h2>
+                <p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+            </article>
+              <hr>
+            <article>
+                <h2>Google Chrome</h2>
+                <p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+            </article>
+              <hr>
+            <article>
+                <h2>Google Chrome</h2>
+                <p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+            </article>
+          </main>  
+            <!-- SIDEBAR (RIGHT) -->
+            <aside class="sidebar-right">
+                <p><a href="">Register</a> in order to view the content.</p>
+            </aside>
+        </div>
+        
+               
+        <!-- FOOTER -->
+        <footer>
+            <p>A PHP & MySQL blog made by <a href="https://www.gerard.ninja">Gerard Castrelo Saperas</a>.</p>
+        </footer>
+        
     </body>
 </html>
