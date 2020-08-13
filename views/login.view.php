778c5f490d92b236ad61784ce41d5839e3396819
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-13 14:16:42 +0200

    Removed minlength and autofocus from password at login

--- views/login.view.php
+++ views/login.view.php
@@ -6,7 +6,7 @@
     <input type="email" name="email" required autofocus /><br>
             
     <label for="password">Password</label><br>
-    <input type="password" name="password" minlength="8" placeholder="8 characters or more..." required autofocus pattern="[A-Za-z0-9]+" /><br>
+    <input type="password" name="password" required pattern="[A-Za-z0-9]+" /><br>
             
     <input type="submit" value="Login">
 </form>
