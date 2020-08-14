608499bf8e109965c21282e0da6824d244ca7fe6
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-14 15:09:23 +0200

    Conditional rendering on login

--- index.php
+++ index.php
@@ -18,9 +18,12 @@
             </article>
           </main>  
             <!-- SIDEBAR (RIGHT) -->
-            <aside class="sidebar-right">
-                <p>You need to be a <a href="./views/register.view.php">registered</a> user in order to view the content.</p>
-            </aside>
-        </div>
+            <?php
+            if(!$logged_in){
+                echo "<aside class='sidebar-right'>"
+                    ."<p>You need to be a <a href='./views/register.view.php'>registered</a> user in order to view the content.</p>"
+                    ."</aside>";
+                }
+            ?>
         
 <?php require_once 'includes/footer.php'; ?>
