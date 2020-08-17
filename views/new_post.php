9a0b02a8e12e31ce5187683b5ba94b74d8096727
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-17 10:04:14 +0200

    Can add posts sucessfully

--- views/new_post.php
+++ views/new_post.php
@@ -25,10 +25,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $category = isset($_POST['category']) ? test_input(filter_input(INPUT_POST, 'category')) : false;
     
+    $user_id = $_SESSION['user']['id'];
+    
+    // Check if a post with the same title already exists
+    $check_repeated = "SELECT post_title FROM posts WHERE post_title = $title";
+    
+    $repeated = mysqli_query($db, $check_repeated);
+    
+    if ($repeated) {
+        echo "<p>A post with this title already exists</p>";
+        die();
+    }
+    
     // Insert data from POST request into posts table
-    $new_post = "INSERT INTO posts VALUES(NULL, '$username', '$category', '$title', '$body', CURDATE())";
+    $new_post = "INSERT INTO posts VALUES(NULL, '$user_id', '$category', '$title', '$body', CURDATE())";
 
     $insert_new_post = mysqli_query($db, $new_post);
+    
+    // If user inserted correctly...Else...
+    if($insert_new_post) {
+        echo "<p>New post uploaded successfully</p>";
+    } else {
+        if (mysqli_error($db) === "Duplicate entry '$email' for key 'email'") {
+            echo "<p>A user with this email already exists.</p>";
+        } else {
+            echo "<p>Error" . mysqli_error($db) . "</p>";
+        }
+    }
 };
 
 // If a non-logged in user tries to post...
